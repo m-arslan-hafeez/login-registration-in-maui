c6ee45725191ac394c01607d23e4d347aeb5b0fe
m-arslan-hafeez <arslanhafeez1211@gmail.com>
2024-07-12 11:48:35 -0400

    UI Commit

--- AppUI/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/AppUI.AssemblyInfo.cs
+++ AppUI/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/AppUI.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3bc6a0aa9dfe1c84b625173290033eea58130d08")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
